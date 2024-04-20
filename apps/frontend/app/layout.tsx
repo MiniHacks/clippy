280beb2effe17307cb42724f88a1e5c156d88acd
nathanjzhao <43712744+nathanjzhao@users.noreply.github.com>
2024-04-19 23:17:45 -0700

    🗣️  in process of fixing ui

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { Toaster } from "@/components/ui/toaster"
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Toaster />
     </html>
   );
 }
