5250c26b3a9ad87812a83b8329addfe3ff5bcfb0
minizou <gh.mini.zou@gmail.com>
2024-04-20 07:48:38 -0700

    🧼 prompt is shorter

--- apps/frontend/components/ui/prompt.tsx
+++ apps/frontend/components/ui/prompt.tsx
@@ -1,7 +1,6 @@
 import { CornerDownLeft } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
-import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
 
 export default function Prompt() {
@@ -9,13 +8,10 @@ export default function Prompt() {
         <form
             className="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring"
         >
-            <Label htmlFor="message" className="sr-only">
-                Message
-            </Label>
             <Textarea
                 id="message"
                 placeholder="No commentary mukbang but if MrBeast made it"
-                className="resize-none border-0 p-3 shadow-none focus-visible:ring-0"
+                className="min-h-2 max-h-8 overflow-hidden resize-none border-0 p-3 shadow-none focus-visible:ring-0"
             />
             <div className="flex items-center p-3 pt-0">
                 <Button type="submit" size="sm" className="ml-auto gap-1.5">
