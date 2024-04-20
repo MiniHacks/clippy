6f9e7bdcca1a623ebf457bdc0c9431dcfaf6bde2
Samyok Nepal <sam@yok.dev>
2024-04-19 21:28:30 -0700

    🥸 create folders for input/output

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -1,1 +1,14 @@
+import subprocess
+
 print("hi sasha :)")
+
+
+def ffmpeg(args):
+    """
+    call ffmpeg with the given arguments
+    """
+    subprocess.run(["ffmpeg"] + args)
+
+
+def main():
+    ffmpeg(["-i", "input.mp4", "output.mp4"])
