2196c32ef172f040d6b978f8be1b6e6658f997f9
Samyok Nepal <sam@yok.dev>
2024-04-20 11:33:36 -0700

    🍈 wup

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -3,19 +3,26 @@ from utils import preview
 
 
 def main():
-    input_file = "dinnerandline"
+    input_file = "dinner"
 
     processor = Processor()
 
-    a = processor.clip(input_file, 0, 5, "a")
+    # a = processor.clip(input_file, 0, 5, "a")
+    #
+    # b = processor.clip(input_file, 10, 15, "b")
+    #
+    # processor.cross_fade(a, b, "output")
 
-    b = processor.clip(input_file, 10, 15, "b")
+    input_dur = processor.duration(input_file)
 
-    processor.cross_fade(a, b, "output")
+    start = processor.clip(input_file, start=input_dur - 30, end=input_dur - 15, output="start")
+    end = processor.clip(input_file, start=input_dur - 15, output="end")
+
+    output = processor.cross_fade(start, end, "output")
 
     # processor.combine(a, b, output="output")
     # preview the output
-    preview("videos/output.mp4")
+    preview(f"videos/{output}.mp4")
 
 
 if __name__ == "__main__":
--- apps/backend/processor.py
+++ apps/backend/processor.py
@@ -25,8 +25,8 @@ class Processor:
         """
 
         print(f"Clipping video from {start} to {end}")
-        outfile = f"{self.dir}/{output}.mp4"
-        infile = f"{self.dir}/{a}.mp4"
+        outfile = f"{self.dir}/{output}.mkv"
+        infile = f"{self.dir}/{a}.mkv"
         args = ([
                     "-ss", str(start),
                 ]
@@ -50,8 +50,8 @@ class Processor:
         """
         print(f"Clipping the last {seconds} seconds of video {a}")
 
-        infile = f"{self.dir}/{a}.mp4"
-        outfile = f"{self.dir}/{output}.mp4"
+        infile = f"{self.dir}/{a}.mkv"
+        outfile = f"{self.dir}/{output}.mkv"
 
         args = [
             "-sseof", str(-seconds),
@@ -77,7 +77,7 @@ class Processor:
             "format=duration",
             "-of",
             "default=noprint_wrappers=1:nokey=1",
-            f"{self.dir}/{a}.mp4",
+            f"{self.dir}/{a}.mkv",
         ]
 
         return float(subprocess.run(args, capture_output=True).stdout.decode("utf-8"))
@@ -92,34 +92,45 @@ class Processor:
         """
         # first, clip the two videos to -0.5s from the end of the first and the 0.5s of the start of the next
         dur_a = self.duration(a)
-        fade_dur = 2
+        fade_dur = 5
+
         start_a = self.clip(a, 0, dur_a - fade_dur, "tmp/start_a")
-        end_a = self.clip_end(a, fade_dur, "tmp/clip_end_a")
+        end_a = self.clip(a, start=dur_a - fade_dur, output="tmp/clip_end_a")
 
         start_b = self.clip(b, 0, fade_dur, "tmp/start_b")
         end_b = self.clip(b, start=fade_dur, output="tmp/clip_end_b")
 
-        # cross-fade the two videos
-
         args = [
-            "-o", f"{self.dir}/tmp/combined.mp4"
-            f"{self.dir}/{end_a}.mp4",
-            f"{self.dir}/{start_b}.mp4",
-            # "-filter_complex", f"xfade=transition=fade:duration={fade_dur}:offset={fade_dur / 2}",
-            # "-timecode", "00:00:00:00",
-            # "-c", "copy",
-            # "-map", "[v]",
-            # "-vcodec", "libvpx-vp9"
+            "-vsync", "0",
+            "-i", f"{self.dir}/{end_a}.mkv",
+            "-i", f"{self.dir}/{start_b}.mkv",
+            "-filter_complex",
+            "[0]settb=AVTB[0:v];[1]settb=AVTB[1:v];[0]aresample=async=1:first_pts=0,apad,atrim=0:5[0:a];[1]aresample=async=1:first_pts=0,apad,atrim=0:5[1:a];[0:v][1:v]xfade=transition=fade:duration=1:offset=4,format=yuv420p[video];[0:a][1:a]acrossfade=d=1:c1=tri:c2=tri[audio]",
+            "-map", "[audio]",
+            "-ar", "48000",
+            "-b:a", "96k",
+            "-map", "[video]",
+            # "-rc", "vbr",
+            # "-cq", "30",
+            "-qmin", "30",
+            "-qmax", "30",
+            f"{self.dir}/tmp/_combined.mkv",
         ]
 
-        print("Cross fading video from {} to {}".format(end_a, start_b))
-        ffmpeg_concat(args)
+        ffmpeg(args)
 
-        print(f"cross faded and saved to {self.dir}/tmp/combined.mp4")
+        # cross-fade the two videos
 
-        # combine all into outfile
-        self.combine(start_a, "tmp/combined", end_b, output=output)
-
+        # print("Cross fading video from {} to {}".format(end_a, start_b))
+        # ffmpeg_concat(args)
+        #
+        print(f"cross faded and saved to {self.dir}/tmp/_combined.mkv")
+        print(f"Combining videos {start_a} and {end_b} with cross-faded _combined.mkv")
+        #
+        # # combine all into outfile
+        self.combine(start_a, "tmp/_combined", end_b, output=output)
+        #
+        print(f"Cross-faded video saved to {self.dir}/{output}.mkv")
         return output
 
     def combine(self, *videos, output="output"):
@@ -129,14 +140,14 @@ class Processor:
         print(f"Combining videos {videos}")
 
         for vid in list(videos):
-            print(f"file '{self.dir}/{vid}.mp4'", file=open("concat.txt", "a"))
+            print(f"file '{self.dir}/{vid}.mkv'", file=open("concat.txt", "a"))
 
         args = [
             "-f", "concat",
             # "-safe", "0",
             "-i", "concat.txt",
             "-c", "copy",
-            f"{self.dir}/{output}.mp4"
+            f"{self.dir}/{output}.mkv"
         ]
 
         ffmpeg(args)
@@ -144,5 +155,5 @@ class Processor:
         # clean up
         os.remove("concat.txt")
 
-        print(f"Videos combined and saved to {self.dir}/output.mp4")
+        print(f"Videos combined and saved to {self.dir}/output.mkv")
         return "output"
