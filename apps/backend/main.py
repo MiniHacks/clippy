92f496efbc71ea3c203f491063177666f5def734
minizou <gh.mini.zou@gmail.com>
2024-04-20 20:26:08 -0700

    🦑 upload video

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -1,7 +1,15 @@
-from processor import Processor
-from utils import preview
-from typing import Union, Annotated
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, UploadFile
+from videohash import VideoHash
+from gemini import upload_file
+import redis
+import os
+
+r = redis.Redis(
+    host=os.environ['REDIS_HOST'],
+    port=33171,
+    password=os.environ['REDIS_PASSWORD'],
+    ssl=True
+)
 
 app = FastAPI()
 
@@ -10,12 +18,16 @@ app = FastAPI()
 async def read_root():
     return {"Hello": "World"}
 
-# do we wanna load in chunks?
-@app.post("/videos/")
-async def create_file(file: Annotated[bytes, File()]):
-    return {"file_size": len(file)}
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
+# do we wanna load in chunks? + md5?
+@app.post("/upload/")
+async def upload_video(upload: UploadFile):
+    file_path = os.path.realpath(upload.file.name)
+    hashed_vid = VideoHash(url=file_path).hash
 
+    if not r.get(hashed_vid):
+        await r.set(hashed_vid, file_path)
+        upload_file(file_path)
+    os.rename(file_path, f"./videos/{hashed_vid}")
+
+    return {"filename": upload.filename}
